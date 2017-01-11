9e7e447273ad080a5294ce68be76b01c5ee9bcb9
Yuliya <ylobukhova@gmail.com>
2017-01-11 10:23:57 +0300

    the end

--- src/js/script.js
+++ src/js/script.js
@@ -96,3 +96,13 @@ $('#prize__list-more').on('click', function(e) {
 });
 
 });
+
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
