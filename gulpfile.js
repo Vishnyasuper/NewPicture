e1498b87383da2647dfb228bb1321cf793b1325c
Yuliya <ylobukhova@gmail.com>
2017-01-11 10:44:52 +0300

    the end1

--- gulpfile.js
+++ gulpfile.js
@@ -113,7 +113,6 @@ gulp.task('js', function () {
   return gulp.src([
       dirs.source + '/js/jquery-3.1.0.min.js',
       dirs.source + '/js/jquery-migrate-1.4.1.min.js',
-      dirs.source + '/js/owl.carousel.min.js',
       dirs.source + '/js/script.js',
     ])
     .pipe(concat('script.min.js'))
